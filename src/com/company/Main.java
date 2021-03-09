0ed02b3cdc290b7360e239efb53c028019b51721
Sudeep-Athanti <asudeep811@gmail.com>
2021-03-09 17:41:17 +0000

    sum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+         int x =5;
+         int y = 8;
+        System.out.println(x+y);
         System.out.println("Hello World!");
         System.out.println("Sudeep");
     }
